e7b1ad79431ca4a9c624f3469ce5827c8be6dd1e
Cooper Brislain <cbrislain@gmail.com>
2019-08-26 16:24:07 -0700

    1337 Green Text

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,24 +8,29 @@ var wordsArray = [
 
 var theWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 var triedLetters = [];
+var tries = 10;
 
 console.log(theWord);
 
 for (var i=0; i<theWord.length; i++) {
     var node = document.createElement("li");
-    node.innerHTML = theWord[i];
+    node.innerHTML = theWord[i].toUpperCase();
     document.querySelector('.letters').appendChild(node);
 }
 
+document.querySelector('.tries_display').innerHTML = `You have ${tries} guesses.`;
+
 document.addEventListener('keydown', (e) => {
     console.log(e.key);
-    rx = new RegExp(/[a-z]/);
+    rx = new RegExp(/^[a-z]$/);
     if(e.key.match(rx)) {
         console.log('alpha');
         if (triedLetters.indexOf(e.key) == -1) {
             triedLetters.push(e.key);
+            var tries_remaining = tries-triedLetters.length;
+            document.querySelector('.tries_display').innerHTML = (tries_remaining? `You have ${tries_remaining} guesses remaining.` : 'You have failed.');
             var node = document.createElement('li');
-            node.innerHTML = e.key;
+            node.innerHTML = e.key.toUpperCase();
             document.querySelector('.tried_letters').appendChild(node);
             var node = document.createElement("li");
             for (i=0; i<theWord.length;i++) {
