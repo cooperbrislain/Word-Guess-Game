468a3cac7a68ea11386926f6236027f5f0eaab76
Cooper Brislain <cbrislain@gmail.com>
2019-08-27 10:58:38 -0700

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,7 +55,8 @@ function terminal_log(message) {
 
 var WordGame = {
     theWord,
-    tries,
+    tries: 10,
+    triedLetters: [],
     start: function() {
         // initialize
         var letterContainer = document.querySelector('.letters');
@@ -81,11 +82,50 @@ var WordGame = {
     },
     win: function() {
 
+    },
+    check_letter: function(letter) {
+        var letterContainer = document.querySelector('.letters');
+        if (this.triedLetters.indexOf(letter) == -1) {
+            this.triedLetters.push(letter);
+            terminal_log(`Trying character: '${letter}'`);
+            var found_count = 0;
+            for (i=0; i<this.theWord.length;i++) {
+                var letterNode = letterContainer.childNodes[i+1];
+                if (!letterNode.classList.contains('cracked')) {
+                    letterNode.innerHTML = '*';
+                    setTimeout(function(letterNode) {
+                        if(!letterNode.classList.contains('cracked')) {
+                            letterNode.innerHTML = '&nbsp;';
+                        }
+                    },250,letterNode);
+                }
+                if (this.theWord[i] == e.key) {
+                    found_count++;
+                    letterNode.innerHTML = theWord[i].toUpperCase();
+                    letterNode.classList.add('cracked');
+                }
+            } 
+            if (found_count) {
+                terminal_log(`hash for ${e.key} found ${found_count} time${found_count>1?'s':''}`);
+                if (!document.querySelector('.letters li:not(.cracked)')) {
+                    terminal_log(`YOU HACKED THE GIBSON!`);
+                }
+            } else {
+                terminal_log('Pattern not found.');
+                this.tries--;
+                if (this.tries == 0) {
+                    terminal_log('YOU FAIL');
+                } else {
+                    terminal_log(`You have ${this.tries} failed attempt${this.tries>1?'s':''} remaining.`);
+                }
+            }
+        } else {
+            terminal_log('Character already cracked');
+        }
     }
 };
 
 document.addEventListener('keydown', (e) => {
-    console.log(e.key);
     switch (e.key) {
         case 'Enter':
             break;  
@@ -97,43 +137,7 @@ document.addEventListener('keydown', (e) => {
         default:
             rx = new RegExp(/^[a-z]$/);
             if(e.key.match(rx)) {
-                if (triedLetters.indexOf(e.key) == -1) {
-                    triedLetters.push(e.key);
-                    terminal_log(`Trying character: '${e.key}'`);
-                    var found_count = 0;
-                    for (i=0; i<theWord.length;i++) {
-                        var letterNode = document.querySelector('.letters').childNodes[i+1];
-                        if (!letterNode.classList.contains('cracked')) {
-                            letterNode.innerHTML = '*';
-                            setTimeout(function(letterNode) {
-                                if(!letterNode.classList.contains('cracked')) {
-                                    letterNode.innerHTML = '&nbsp;';
-                                }
-                            },250,letterNode);
-                        }
-                        if (theWord[i] == e.key) {
-                            found_count++;
-                            letterNode.innerHTML = theWord[i].toUpperCase();
-                            letterNode.classList.add('cracked');
-                        }
-                    } 
-                    if (found_count) {
-                        terminal_log(`hash for ${e.key} found ${found_count} time${found_count>1?'s':''}`);
-                        if (!document.querySelector('.letters li:not(.cracked)')) {
-                            terminal_log(`YOU HACKED THE GIBSON!`);
-                        }
-                    } else {
-                        terminal_log('Pattern not found.');
-                        tries--;
-                        if (tries == 0) {
-                            terminal_log('YOU FAIL');
-                        } else {
-                            terminal_log(`You have ${tries} failed attempt${tries>1?'s':''} remaining.`);
-                        }
-                    }
-                } else {
-                    terminal_log('Character already cracked');
-                }
+                WordGame.check_letter(e.key);
             } else {
                 terminal_log('Not a valid character');
             }
